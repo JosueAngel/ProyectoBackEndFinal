9f913446a4ffdc9c82acab92587806d8bde79433
JOSUE ANGEL <JOSUE ANGEL@LAPTOP-8N0FQEI5>
2020-11-06 11:25:25 -0600

    Proyecto Back End terminado

--- src/main/java/Proyecto/ProyectoBackEndApplication.java
+++ src/main/java/Proyecto/ProyectoBackEndApplication.java
@@ -2,8 +2,11 @@ package Proyecto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+
 public class ProyectoBackEndApplication {
 
 	public static void main(String[] args) {
